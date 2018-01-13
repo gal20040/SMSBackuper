e40ab1dbf09fae145f02a303bdf9b4531cca39ab
gal20040 <gal20040@gmail.com>
2018-01-14 00:40:36 +0300

    вытащил обработку exception в отдельный класс.

---
+++ app/src/main/java/ru/gal20040/smsbackuper/ExceptionHandler.java
@@ -0,0 +1,21 @@
+package ru.gal20040.smsbackuper;
+
+import android.util.Log;
+
+class ExceptionHandler {
+    static final String LOG_TAG = "myLogs";
+
+    void errorAction(Exception ex) {
+        Log.d(LOG_TAG, getStackTrace(ex));
+    }
+
+    private static String getStackTrace(Exception ex) {
+        StringBuilder sb = new StringBuilder(500);
+        StackTraceElement[] st = ex.getStackTrace();
+        sb.append(ex.getClass().getName()).append(": ").append(ex.getMessage()).append("\n");
+        for (StackTraceElement aSt : st) {
+            sb.append("\t at ").append(aSt.toString()).append("\n");
+        }
+        return sb.toString();
+    }
+}
